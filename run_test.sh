1c0290a8c2dd1fdc9b4cd8bf1d410a36d1c8fe26
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2021-11-24 09:34:02 +0100

    Output from tests to file, handle SIGSEGV and SIGABRT

--- run_test.sh
+++ run_test.sh
@@ -7,51 +7,88 @@ fi
 
 BASIC_TESTS="tests/input"
 IFJ_TESTS="tests/input_ifj"
+LOG="tests/tests_output.log"
+rm $LOG
+touch $LOG
 
-echo "------------ LEX testy ------------"
-echo ""
+
+function to_file()
+{
+	echo $1 >> $LOG	
+}
+
+
+to_file "------------ LEX testy ------------"
+to_file ""
 lines=("%Ahoj" "&Ahoj" "89e" "89e+" "89e-" "8ea9")
 LEX_OK=("ahoj" "_cau" "AhoK_d" "ahoj_98" "98" "08.0" "98.0") 
 for each in "${lines[@]}"; do
-	echo "Vstup: $each"
+	to_file "Vstup: $each"
 	echo $each |./$EXECUTABLE
 	if [ $? -eq 1 ]; then
-		echo "Chybovy kod - OK"
+		to_file "Chybovy kod - OK" 
 	else
-		echo "Chybovy kod - X"
-		echo "vas chybovy kod: $?"
+		to_file "Chybovy kod - X"
+		to_file "vas chybovy kod: $?"
 	fi
 done
+
 for each in "${LEX_OK[@]}"; do
-	echo "Vstup: $each"
+	to_file "Vstup: $each"
 	echo $each |./$EXECUTABLE
 	if [ $? -eq 2 ]; then
-		echo "Chybovy kod - OK"
+		to_file "Chybovy kod - OK"
 	else
-		echo "Chybovy kod - X"
-		echo "vas chybovy kod: $?"
+		to_file "Chybovy kod - X"
+		to_file "vas chybovy kod: $?"
 	fi
 done
 
 
-echo ""
-echo "------------ Basic testy ------------"
-echo "Vsetky by mali skoncit s chybovym kodom 0!"
-echo ""
+to_file ""
+to_file "------------ Basic testy ------------"
+to_file "Vsetky by mali skoncit s chybovym kodom 0!"
+to_file ""
 for i in `seq 1 12`; do
 	./$EXECUTABLE < $BASIC_TESTS$i
-	if [ $? -eq 0 ]; then
-		echo "-- test c.$i - OK"
-	else
-		echo "test c.$i - X"
-		echo "ocakavany vystup: 0, vas vystup: $?"
-	fi
+	ERROR_CODE=$? 
+	case $ERROR_CODE in
+		0)
+	  to_file "-- test c.$i - OK"
+	  ;;
+		139)
+	  to_file "-- test c.$i - X (SIGSEGV)"
+	  ;;
+		134)
+	  to_file "-- test c.$i - X (SIGABRT)"
+	  ;;
+		*)
+	  to_file "-- test c.$i - X"
+	  to_file "ocakavany vystup: 0, vas vystup: $ERROR_CODE"
+	  ;;
+	esac
 done
 
-echo ""
-echo "---------- Testy zo zadania ifj projektu ----------"
-echo "Vsetky by mali skoncit s chybovym kodom 0!"
-echo ""
-#for i in `seq 1 5`; do
-#	./$EXECUTABLE < $IFJ_TESTS$i
-#done
+to_file ""
+to_file "---------- Testy zo zadania ifj projektu ----------"
+to_file "Vsetky by mali skoncit s chybovym kodom 0!"
+to_file ""
+for i in `seq 1 3`; do
+	./$EXECUTABLE < $IFJ_TESTS$i	
+	ERROR_CODE=$? 
+	case $ERROR_CODE in
+		0)
+	  to_file "-- test c.$i - OK"
+	  ;;
+		139)
+	  to_file "-- test c.$i - X (SIGSEGV)"
+	  ;;
+		134)
+	  to_file "-- test c.$i - X (SIGABRT)"
+	  ;;
+		*)
+	  to_file "-- test c.$i - X"
+	  to_file "ocakavany vystup: 0, vas vystup: $ERROR_CODE"
+	  ;;
+	esac
+done
