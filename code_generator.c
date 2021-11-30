85a7ea2e87678de6138cb70952dc6277d1353d0f
Ivo Procházka <prochazka.ivo19@gmail.com>
2021-11-30 20:58:58 +0100

    function generate_code

--- code_generator.c
+++ code_generator.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define n_plus_0 2 // need to alocate space for characters \n and \0 
 
 // reads, readi, readn -> READ
 // write -> WRITE * n podla parametrov
@@ -44,7 +49,27 @@
 // kontrola neocakavanej hodnoty nil pri operaciach
 // okrem == ~=
 
-int main(){
-    printf("ahoj");
+void generate_code(char *string, char *code, bool flag)
+{
+    if (flag){
+        string = (char *)realloc(string, strlen(code)*sizeof(char) + n_plus_0);
+        strcat(string, code);
+        strcat(string, "\n");
+    }            
+    else
+    {
+        printf("%s\n", code);
+    }
+}
+
+int main(){    
+    char *while_string = (char *)malloc(sizeof(char));
+    generate_code(while_string, "Lenovo", true);
+    generate_code(while_string, "320", true);
+    generate_code(while_string, "ideapad", true);
+    generate_code(while_string, "Trust", true);    
+    generate_code(while_string, "dawd", true);
+    generate_code(while_string, "gwfqw", true);
+    printf("%s", while_string);
     return 0;
 }
