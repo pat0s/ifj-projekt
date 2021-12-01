994e4d848d99e6f1b57703f2f89bc1495df1d79b
Ivo Procházka <prochazka.ivo19@gmail.com>
2021-12-01 14:31:40 +0100

    neco

--- code_generator.c
+++ code_generator.c
@@ -8,51 +8,7 @@
 
 #define ending_0 2 // need to alocate space for \0
 
-// reads, readi, readn -> READ
-// write -> WRITE * n podla parametrov
-
-// Start
-
-// hlavicka funkcie
-
-// return values
-// return funkcie (prikaz return)
-
-// priradenie hodnot z return funkcie (samotny prikaz return)
-
-// volanie funkcie
-
-// vkladanie parametrov do funckie
-
-// if label
-
-// if jump
-
-// else jmp, label
-
-// if end
-
-// while - hlavicka, telo, end
-
-// premenna vo while !!!!
-
-// var definicia
-
-// var priradenie
-
-// konstanta
-
-// operacie
-// # -> STRLEN
-// .. -> CONCAT
-
-// kontrola delenia 0
-// iba pri / a //
-
-// kontrola neocakavanej hodnoty nil pri operaciach
-// okrem == ~=
-
-void dyn_string_init(TDynString *dyn_string)
+/*void dyn_string_init(TDynString *dyn_string)
 {
     dyn_string->string = NULL;
     dyn_string->length = 0;
@@ -75,21 +31,13 @@ int dyn_string_realloc(TDynString *dyn_string, char *code)
     if (dyn_string->string == NULL) return INTERNAL_ERROR; 
     
     return 0;
-}
-
-
+}*/
 
 void generate_code(TDynString *dyn_string, char *code, bool flag)
 {
     if (flag){
         // Reallocation
-        int error = dyn_string_realloc(dyn_string, code);
-        
-        // Internal error during reallocation
-        if (error == INTERNAL_ERROR)
-        {
-            // TODO nejako to poriesit
-        }
+        int error = dyn_string_realloc(dyn_string, code);       
 
         // Concatenation
         strcat(dyn_string->string, code);        
@@ -127,26 +75,33 @@ void IDIVS(TDynString *string, bool flag)
 
 void CONCAT(TDynString *dyn_string, bool flag, char *var, char *symb1, char *symb2)
 {
-    int concat_len = 6; // delka concat
+    int concat_len = 6; // delka CONCAT
     int var_len = strlen(var);
     int symb1_len = strlen(symb1);
     int symb2_len = strlen(symb2);
-    int spaces_n_0 = 5; // 3 mezery, /n a /0
-    int length = concat_len + var_len + symb1_len + symb2_len + spaces_n_0;
+    int spaces = 3;     // 3 mezery
+    int n_0 = 2;        // 1x/n a 1x/0
+
+    int length = concat_len + var_len + symb1_len + symb2_len + spaces + n_0;
     char *buffer = (char *)malloc(sizeof(char) * length);
-    snprintf(buffer, sizeof(char) * length, "CONCAT %s %s %s\n", var, symb1, symb2);
-    printf("%s", buffer);
+    snprintf(buffer, sizeof(char) * length, "CONCAT %s %s %s\n", var, symb1, symb2);    
     generate_code(dyn_string, buffer, flag);
     free(buffer);
 }
 
-void STRLEN(TDynString *string, bool flag, char *var, char *symb)
+void STRLEN(TDynString *dyn_string, bool flag, char *var, char *symb)
 {
-    generate_code(string, "STRLEN ", flag);
-    generate_code(string, var, flag);
-    generate_code(string, " ", flag);
-    generate_code(string, symb, flag);
-    generate_code(string, "\n", flag);
+    int concat_len = 6; // delka STRLEN
+    int var_len = strlen(var);
+    int symb_len = strlen(symb);    
+    int spaces = 2;     // 2 mezery
+    int n_0 = 2;        // 1x/n a 1x/0
+
+    int length = concat_len + var_len + symb_len + spaces + n_0;
+    char *buffer = (char *)malloc(sizeof(char) * length);
+    snprintf(buffer, sizeof(char) * length, "STRLEN %s %s\n", var, symb);
+    generate_code(dyn_string, buffer, flag);
+    free(buffer);
 }    
 
 void START_AND_BUILTIN_FUNCTIONS()
@@ -171,7 +126,7 @@ void PARAMETERS(char *func_name, char *param_name, int number)
 void RETVALS(char *func_name, int number)
 {
     printf("DEFVAR LF@%s_retval%d\n", func_name, number);
-    printf("MOVE LF@%s_retval%d nil@nil\n", func_name, number);
+    printf("MOVE LF@%s_retval%d nil@nil\n", func_name, number); 
 }
 
 void RETURNS(char *func_name, int number, char *symb)
@@ -186,21 +141,56 @@ void FUNC_END(char *func_name)
     printf("LABEL %s_end\n", func_name);
 }
 
+int c_digits(int n)
+{
+    int r = 1;
+    if(n < 9){
+        n /= 10;
+        r++;
+    }
+    return r;
+}
+
+void WHILE_START(TDynString *string, bool flag, int number)
+{
+    char *s_number = (char *)malloc(sizeof(char) * (c_digits(number) + 1));
+    sprintf(s_number, "%d", number);
+
+    generate_code(string, "DEFVAR LF@T", flag);   
+    generate_code(string, s_number, flag);
+    generate_code(string, "l\n", flag);
+
+    generate_code(string, "DEFVAR LF@T", flag);
+    generate_code(string, s_number, flag);
+    generate_code(string, "r\n", flag);
+
+    generate_code(string, "LABEL", flag);
+    generate_code(string, s_number, flag);
+    generate_code(string, "while\n", flag);
+}
+
+void WHILE_END(TDynString *string, bool flag, int number)
+{
+    char *s_number = (char *)malloc(sizeof(char) * (c_digits(number) + 1));
+    sprintf(s_number, "%d", number);
+
+    generate_code(string, "JUMP ", flag);
+    generate_code(string, s_number, flag);
+    generate_code(string, "while\n", flag);
+
+    generate_code(string, "LABEL ", flag);
+    generate_code(string, s_number, flag);
+    generate_code(string, "end_while\n", flag);
+}
+
 int main()
 {
     // Asi bude potrebna aj alokacia struktury, kedze sa to bude pouzivat vsade
     // to pridam zajtra ked tak
     TDynString dyn_string;
-    dyn_string_init(&dyn_string);
-    ADDS(&dyn_string, true);
-    FUNC_START("foo");
-    PARAMETERS("foo", "a", 1);
-    PARAMETERS("foo", "b", 2);
-    PARAMETERS("foo", "c", 3);
-    RETVALS("foo", 1);
-    RETURNS("foo", 1, "c");
-    FUNC_END("foo");    
+    dyn_string_init(&dyn_string);  
 
+    STRLEN(&dyn_string, true, "baVVV", "biDADda");
     CONCAT(&dyn_string, true, "ba", "bida", "oda");
 
     printf("%s", dyn_string.string);
